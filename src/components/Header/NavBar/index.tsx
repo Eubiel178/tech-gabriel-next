9009af520eab7dab552e8469fb1a23b527a02955
Gabriel <dev123gabriel@gmail.com>
2023-12-15 14:56:08 -0300

    component header e navbar criado

---
+++ src/components/Header/NavBar/index.tsx
@@ -0,0 +1,23 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <menu className="">
+        <li>
+          <a href="#about-me__section">Sobre mim</a>
+        </li>
+
+        <li>
+          <a href="#skills__section">Habilidades</a>
+        </li>
+
+        <li>
+          <a href="#projects__section">Projetos</a>
+        </li>
+
+        <li>
+          <a href="#contact-forms__section">Contatos</a>
+        </li>
+      </menu>
+    </nav>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./NavBar";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>
+        G<span className="">abriel</span>
+      </h1>
+
+      <NavBar />
+    </header>
+  );
+};
