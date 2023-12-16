af1197d59b6cf0b1789e9907cb5c9d49ed340d28
Gabriel <dev123gabriel@gmail.com>
2023-12-16 13:09:24 -0300

    refactor: class se repetindo no developer info

--- src/components/Sections/DeveloperInfo/index.tsx
+++ src/components/Sections/DeveloperInfo/index.tsx
@@ -4,18 +4,16 @@ import Image from "next/image";
 
 export const DeveloperInfo = () => {
   return (
-    <section className="relative  centered bg-shadow h-[37rem]">
+    <section className="relative centered bg-shadow h-[37rem]">
       <Image
-        className="absolute top-0 h-full z-[-1]  content-width"
+        className="absolute top-0 h-full z-[-1] content-width"
         src={banner}
         alt="banner"
       />
 
-      <div className="container-config z-10 container-config">
+      <div className="container-config content-width items-center z-10">
         <Image
-          className="border-solid border-4 border-gray-100 rounded-full h-44 w-44	
-
-	"
+          className="border-solid border-4 border-gray-100 rounded-full h-44 w-44"
           src={developer}
           alt="Foto do desenvolvedor: Gabriel Santos"
         />
