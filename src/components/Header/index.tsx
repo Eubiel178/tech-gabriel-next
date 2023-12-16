d6a43aa0c8cbdad12ba6230c85d8faec1b43fc32
Gabriel <dev123gabriel@gmail.com>
2023-12-16 17:12:54 -0300

    refactor: css do app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "./NavBar";
 
 export const Header = () => {
   return (
-    <header className="bg-secondary py-9 px-5 centered">
+    <header className="fixed z-20 inset-x-0	bg-secondary py-5 px-5 centered">
       <div className="content-width flex justify-between items-center content-width">
         <h1 className="title-1 ">
           G<span className="text-highlight">abriel</span>
